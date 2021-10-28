ba573e517e556e696aa5564e9e3358cb3c71dc8c
S <s>
2021-10-28 11:13:06 +0200

    {"type":"TI", "runs":"0"}

---
+++ WindowCleaningKarel.java
@@ -0,0 +1,27 @@
+import stanford.karel.SuperKarel;
+
+/**
+ * MIT License (http://choosealicense.com/licenses/mit/)
+ * 
+ * Karel: 7. WindowCleaningKarel
+ * 
+ * Karel's day job is to clean windows of skyscrapers in Chicago. He is facing
+ * east, standing in front of the first skyscraper.
+ * 
+ * @see http://www.VariationenZumThema.de/
+ * @author Ralph P. Lano
+ */
+public class WindowCleaningKarel extends SuperKarel {
+
+	/**
+	 * Karel has to clean five skyscrapers, one at a time.
+	 * 
+	 * PreCondition: Karel is standing in front of the first skyscraper, facing
+	 * east <br/>
+	 * PostCondition: Karel is standing behind the last skyscraper, facing east
+	 */
+	public void run() {
+		// your code goes here...
+		
+	}
+}
