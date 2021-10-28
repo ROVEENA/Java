7e211a03e4fa0fdafbd3a3b0b518bbe56ee67377
S <s>
2021-10-28 10:59:12 +0200

    {"type":"TI", "runs":"3"}

---
+++ AdventureKarel.java
@@ -0,0 +1,19 @@
+import stanford.karel.SuperKarel;
+
+/**
+ * MIT License (http://choosealicense.com/licenses/mit/)
+ * 
+ * Karel: AdventureKarel
+ * 
+ * Karel is on an adventure and has to be careful not to fall off a cliff.
+ * 
+ * @see http://www.VariationenZumThema.de/
+ * @author Ralph P. Lano
+ */
+public class AdventureKarel extends SuperKarel {
+
+	public void run() {
+		// your code goes here...
+			
+	}
+}
---
+++ WallKarel.java
@@ -0,0 +1,19 @@
+import stanford.karel.Karel;
+
+/**
+ * MIT License (http://choosealicense.com/licenses/mit/)
+ * 
+ * Karel: 4. WallKarel
+ * 
+ * Karel is supposed to walk to the wall.
+ * 
+ * @see http://www.VariationenZumThema.de/
+ * @author Ralph P. Lano
+ */
+public class WallKarel extends Karel {
+
+	public void run() {
+		// your code goes here...
+		
+	}
+}
